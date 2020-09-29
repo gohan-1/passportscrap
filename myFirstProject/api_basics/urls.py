f14f784b81ea6639f99f292bfc9e1fdee9e71870
gohan-1 <gohanth@gmail.com>
2020-09-29 11:17:03 +0530

    first commit

---
+++ myFirstProject/api_basics/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from .views import train_model,scrap,predict
+
+
+urlpatterns = [
+
+    path('train/', train_model),
+    path('scrap/', scrap),
+    path('predict/', predict)
+]
