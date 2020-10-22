9b902f74a9e9bea8495cdc4307d5de0bc3907958
gohan-1 <gohanthe@gmail.com>
2020-10-22 22:10:31 +0530

    added fixed req

--- myFirstProject/myFirstProject/settings.py
+++ myFirstProject/myFirstProject/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +127,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+django_heroku.settings(locals())
