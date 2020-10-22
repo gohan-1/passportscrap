2b13450fad5c3f03422eb0f2584bceb2a5958823
gohan-1 <gohanthe@gmail.com>
2020-10-22 16:08:22 +0530

    heroku added chnages

--- myFirstProject/myFirstProject/settings.py
+++ myFirstProject/myFirstProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'jbjirp$mxftrsqz1x8m%y(sjy5-b)ayc3qfqvla56++*42z$0p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
